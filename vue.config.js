86608447f4d93aa733e358a0cac45f01ae8baeed
liwuyi <973574291@qq.com>
2022-11-13 02:28:20 +0800

    add DockerFile

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  parallel: false,
   configureWebpack: {
     module: {
       rules: [
